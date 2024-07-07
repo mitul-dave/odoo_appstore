c15c33b42ae4d12f34858da82a3d8cad1669b275
Mitul Dave <mitul.dave16@gmail.com>
2024-07-07 16:01:58 +0530

    [ADD] new module sale reorder

--- md_sale_reorder/__manifest__.py
+++ md_sale_reorder/__manifest__.py
@@ -17,7 +17,7 @@
         "views/sale_order_inherit.xml",
         "wizard/sale_reorder_view.xml"
     ],
-    "image": ['static/description/banner.png'],
+    "images": ['static/description/banner.png'],
     "installable": True,
     "auto_install": False,
     "application": True,
